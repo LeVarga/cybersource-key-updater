a9e25a1c8f9262306b37808eea545d12db0a3eae
Dylan Vu <dylanvu9@gmail.com>
2024-02-21 00:50:08 -0800

    sidebar almost done but omving on

--- vite-frontend/src/App.tsx
+++ vite-frontend/src/App.tsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
 import Textbox from './components/textbox';
+import Sidebar from './components/sidebar/sidebar';
 import './App.css'
 
-function App() {
+function App({ merchantId, distributorId }: { merchantId: string, distributorId: string }) {
   const [inputs, setInputs] = useState({
     key: '',
     secret: '',
-    dataAcctID: '',
-    distID: ''
   });
 
   const [result, setResult] = useState('');
@@ -45,23 +44,28 @@ function App() {
   };
 
   return (
-    <div className='bg-white'>
-      <h1>Payment Key Validation</h1>
-      <form className="w-full max-w-sm" onSubmit={handleSubmit}>
-        <Textbox name='key' id="inline-key" value={inputs.key} label='Key ID' handleChange={handleChange} />
-        <Textbox name='secret' id="inline-secret" value={inputs.secret} label='Key Secret' handleChange={handleChange} />
-        <div className="md:flex md:items-center">
-          <div className="md:w-1/3"></div>
-          <div className="md:w-2/3">
-            <button
-              className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-              type="submit">
-              Submit
-            </button>
+    <div className='bg-white grid grid-cols-2'>
+      <Sidebar />
+      <div>
+        <h1>Payment Key Validation</h1>
+        <p>{merchantId}</p>
+        <p>{distributorId}</p>
+        <form className="w-full max-w-sm" onSubmit={handleSubmit}>
+          <Textbox name='key' id="inline-key" value={inputs.key} label='Key ID' handleChange={handleChange} />
+          <Textbox name='secret' id="inline-secret" value={inputs.secret} label='Key Secret' handleChange={handleChange} />
+          <div className="md:flex md:items-center">
+            <div className="md:w-1/3"></div>
+            <div className="md:w-2/3">
+              <button
+                className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                type="submit">
+                Submit
+              </button>
+            </div>
           </div>
-        </div>
-      </form>
-      {loading ? <div className="spinner"></div> : <div>{result}</div>}
+        </form>
+        {loading ? <div className="spinner"></div> : <div>{result}</div>}
+      </div>
     </div>
   )
 }
---
+++ vite-frontend/src/components/sidebar/sidebar.tsx
@@ -0,0 +1,15 @@
+import Navlink from "./navlink"
+export default function Sidebar() {
+    const links = ["URL List", "Calendar", "Maintenance", "Install/Upgrades", "Onsales", "Pixel", "Clients", "Evenue Settings", "Customizations", "Dashboard", "Admin", "Payment Keys"]
+    return (
+        <div className="bg-black text-white h-screen">
+            <div className="m-4 w-3/4">
+                <img src="./logo.webp" alt="Paciolan Logo" />
+            </div>
+            {/* now display all the routes */}
+            <nav className="bg-darkGray my-4">
+                {links.map(link => <Navlink text={link} key={link + "-link"} />)}
+            </nav>
+        </div>
+    )
+}
