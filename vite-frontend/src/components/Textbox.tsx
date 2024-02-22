20e10c44cbe4406decfa458aaf5806879469ba15
LeVarga <16280141+LeVarga@users.noreply.github.com>
2024-02-21 23:10:51 -0800

    Add distributor ID search and selection (unfinished), fix error message display.

--- vite-frontend/src/components/Textbox.tsx
+++ vite-frontend/src/components/Textbox.tsx
@@ -1,5 +1,5 @@
 
-export default function Textbox(props: { name: string, id: string, value: string, label: string, handleChange: React.ChangeEventHandler<HTMLInputElement> }): JSX.Element {
+export default function Textbox(props: { name: string, id: string, value: string, label: string, handleChange: React.ChangeEventHandler<HTMLInputElement>, disabled: boolean }): JSX.Element {
     return (
         <div className="mb-8 ml-4">
             <label className="block text-gray-500 font-bold mb-1 text-left"
@@ -10,6 +10,7 @@ export default function Textbox(props: { name: string, id: string, value: string
                 <input
                     className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-black leading-tight focus:outline-none focus:bg-white focus:border-red"
                     name={props.name} id={props.id} type="text" value={props.value} onChange={props.handleChange}
+                    disabled={props.disabled}
                     placeholder={`Input ${props.label}...`}
                 />
             </div>
--- vite-frontend/src/routes/App.tsx
+++ vite-frontend/src/routes/App.tsx
@@ -1,18 +1,23 @@
 import { useState } from 'react'
 import Textbox from '../components/Textbox';
-import Sidebar from '../components/sidebar/Sidebar';
+import Sidebar from "../components/sidebar/Sidebar";
 import './App.css'
 
-export default function App({ dataAcctID, merchantId, distID }: { dataAcctID: string, merchantId: string, distID: string }) {
+export default function App() {
+
   const [inputs, setInputs] = useState({
     key: '',
     secret: '',
-    dataAcctID: dataAcctID,
-    distID: distID
+    dataAcctID: '',
+    sk: '',
+    distIDs: '',
   });
 
-  const [result, setResult] = useState('');
+  const [result, setResult] = useState<{}[]>([])
   const [loading, setLoading] = useState(false);
+  const [resultMessage, setResultMessage] = useState("");
+  const [error, setError] = useState(Error);
+  const [currentStep, setStep] = useState(0);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -25,79 +30,99 @@ export default function App({ dataAcctID, merchantId, distID }: { dataAcctID: st
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setLoading(true); // Start loading
-    fetch('https://yf6zmmg1j0.execute-api.us-west-1.amazonaws.com/Prod', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(inputs),
-    })
-      .then(response => response.json())
-      .then(data => {
-        setResult(JSON.stringify(data));
+
+    if (currentStep == 0) { // step 0 is submitting the client ID
+      const url = new URL('https://yf6zmmg1j0.execute-api.us-west-1.amazonaws.com/Prod/getDistributors');
+      url.searchParams.set('dataAcctID', inputs.dataAcctID);
+      fetch(url, {
+        method: 'GET',
       })
-      .catch((error) => {
-        console.error('Error:', error);
-        setResult(error.message);
+          .then((response) => {
+            response.json().then((json) => {
+              if (!json?.error) {
+                setStep(1);
+                setResult(json.data);
+                setResultMessage("");
+              }
+              else {
+                setResultMessage(json.message);
+                setError(json.error);
+              }
+            })
+          })
+          .catch((error) => {
+            console.error('Error:', error);
+            // Handle error and display user-friendly message
+            setError(error);
+            setResultMessage('An error occurred. Please try again later.');
+          })
+          .finally(() => {
+            setLoading(false);
+          });
+    } else if (currentStep == 1) { // step 1 is selecting the distributor ID(s) to update
+      // TODO: set the values from the selected checkboxes here
+      setStep(2);
+      setLoading(false);
+
+    } else if (currentStep == 2) { // step 2 is getting the key/secret and submitting the update
+      fetch('https://yf6zmmg1j0.execute-api.us-west-1.amazonaws.com/Prod/updateSecret', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(inputs),
       })
-      .finally(() => {
-        setLoading(false);
-      });
+          .then((response) => { // TODO: refactor this into a named function (it's mostly the same as step 0)
+            response.json().then((json) => {
+              if (!json?.error) {
+                setStep(1);
+                setResult(json.data);
+                setResultMessage("");
+              }
+              else {
+                setResultMessage(json.message);
+                setError(json.error);
+              }
+            })
+          })
+          .catch((error) => {
+            console.error('Error:', error);
+            // Handle error and display user-friendly message
+            setError(error);
+            setResultMessage('An error occurred. Please try again later.');
+          })
+          .finally(() => {
+            setLoading(false);
+          });
+    }
   };
 
-  
-  const handleDistributer = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    setLoading(true); // Start loading
 
-    const url = new URL('https://yf6zmmg1j0.execute-api.us-west-1.amazonaws.com/Prod/getDistributors');
-    url.searchParams.set('dataAcctID', inputs.dataAcctID);
+  let distributorCheckboxes = result.map(function(item) {
+     return (<div><input type="checkbox" id={item?.sk + item?.distID} name={item?.distID} data-sk={item?.sk} data-distID={item?.distID}/>
+      <label htmlFor={item?.sk + item?.distID}> {item?.distID}</label>
+      </div>);
+  });
 
-    fetch(url, {
-      method: 'GET', // Assuming this is a GET request
-    })
-      .then(response => response.json())
-      .then(data => {
-        // setResult(JSON.stringify(data));
-        const distIDs = data.data?.map((distributor: { distID: any; }) => distributor.distID) ?? [];
-        const formattedDistIDs = distIDs.join(' ');
-        setResult(formattedDistIDs);
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-        // Handle error and display user-friendly message
-        setResult('An error occurred. Please try again later.');
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  };
-
-
-  return (
-    <div className='bg-white grid grid-cols-2'>
-      <Sidebar />
-      <div className='w-full'>
-        <h1 className='text-left mx-4 pt-4 pb-2'>Payment Key Validation</h1>
-        {/* area containing current ids */}
-        <div className='grid grid-cols-2 items-start my-4'>
-          <span className='bg-lightGray-300 mx-4 w-auto p-1 rounded font-bold'>Merchant ID: {merchantId}</span>
-          <span className='bg-lightGray-200 mx-4 w-auto p-1 rounded'>Distributor {distID }</span>
+    return (
+      <div className='bg-white grid grid-cols-2'>
+        <Sidebar/>
+        <div className='w-full'>
+          <h1 className='text-left mx-4 pt-4 pb-2'>Payment Key Validation</h1>
+          {Textbox({name: "dataAcctID", id: "inline-dataAcctID", value: inputs.dataAcctID, disabled: currentStep != 0, label: "Data Account ID", handleChange})}
+          {currentStep == 2 ? Textbox({name: "key", id: "inline-key", value: inputs.dataAcctID, disabled: false, label: "Key", handleChange}) : null}
+          {currentStep == 2 ? Textbox({name: "secret", id: "inline-secret", value: inputs.dataAcctID, disabled: false, label: "Secret", handleChange}) : null}
+          <form className="w-full" onSubmit={handleSubmit} id="submit">
+            {currentStep == 1 ? distributorCheckboxes : null}
+            <button
+                className="shadow bg-red focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded float-right"
+                type="submit">
+              Validate
+            </button>
+          </form>
+          {loading ? <div className="spinner"></div> : <div>{resultMessage}</div>}
         </div>
-        {/* textbox form */}
-        <form className="w-full" onSubmit={handleSubmit}>
-          <Textbox name='key' id="inline-key" value={inputs.key} label='Key ID' handleChange={handleChange} />
-          <Textbox name='secret' id="inline-secret" value={inputs.secret} label='Key Secret' handleChange={handleChange} />
-          <button
-            className="shadow bg-red focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded float-right"
-            type="submit">
-            Validate
-          </button>
-        </form>
-        {loading ? <div className="spinner"></div> : <div>{result}</div>}
-      <button onClick={handleDistributer}>Handle Distributor</button>
-      </div>
 
-    </div>
+      </div>
   )
 }
