9929b666f2e31e90df336703a6d17baff92a8d76
Henry Reyes <reyesh2@uci.edu>
2024-02-27 13:33:33 -0800

    fix sidebar to match grid

--- vite-frontend/src/components/sidebar/Sidebar.tsx
+++ vite-frontend/src/components/sidebar/Sidebar.tsx
@@ -2,7 +2,7 @@ import Navlink from "./Navlink"
 export default function Sidebar() {
     const links = ["URL List", "Calendar", "Maintenance", "Install/Upgrades", "Onsales", "Pixel", "Clients", "Evenue Settings", "Customizations", "Dashboard", "Admin", "Payment Keys"]
     return (
-        <div className="bg-black text-white h-screen w-1/3">
+        <div className="bg-black text-white h-screen col-span-1">
             <div className="m-4 w-3/4">
                 <img src="./logo.webp" alt="Paciolan Logo" />
             </div>
